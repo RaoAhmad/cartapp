3f4c6432b367d8f4f776efdb4e51bafb4a536b18
RaoAhmad <ahmadv308@gmail.com>
2023-12-23 08:31:06 +0500

    .

---
+++ components/Flatlist.js
@@ -0,0 +1,31 @@
+import {Text, View, StyleSheet, FlatList} from "react-native"
+
+export const Flatlist = () => {
+
+    const employees=[
+        {
+           id:1,
+           name: 'Ashraf',
+},
+{
+    id:2,
+    name: 'Asif',
+
+
+},
+{
+    id:3,
+    name: 'Umer',
+
+
+}
+]
+
+  return (
+   <View>
+<Text>this is flat List</Text>
+<FlatList></FlatList>
+   </View>
+
+  )
+}
