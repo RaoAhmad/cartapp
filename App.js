95521672043d61ba77b859049739a8872ff604df
RaoAhmad <ahmadv308@gmail.com>
2024-01-08 16:48:35 +0500

    add-status

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Flexbox from './components/Flexbox';
+/* import { StatusBar } from 'expo-status-bar'; */
+import { StyleSheet, Text, View ,StatusBar , Platform} from 'react-native';
+/* import Flexbox from './components/Flexbox */
+
 /* import Flatlist from './components/Flatlist';
 import Showhide from './components/Showhide'; */
 export default function App() {
   return (
     <View style={styles.container}>
      {/*  <Text style={{fontSize:30}}>hi expo!</Text> */}
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="purple" />
+      <Text>Platform:{Platform.OS}</Text>
     {/*  <Flatlist/> */}
 {/*  <Showhide/> */}
-<Flexbox/>
+
+{/* <Flexbox/> */}
     </View>
   
 
