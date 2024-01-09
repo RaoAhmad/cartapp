9cf1136aa746aaf838f85bfab23f76eb8981bae7
RaoAhmad <ahmadv308@gmail.com>
2024-01-09 18:42:55 +0500

    api-data-fetch

--- App.js
+++ App.js
@@ -1,47 +1,21 @@
-
-import { StyleSheet, Text, View, Button, FlatList} from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Flatlist from './components/Flatlist';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {Text, View, Button, StyleSheet  } from 'react-native'
 import Homescreen from './components/Homescreen';
+import Flatlist from './components/Flatlist';
 
-
-
-
-
-const Stack = createNativeStackNavigator();
-export default function App() {
-  const app=()=>{
-    console.warn('hello app');
-  }
+const Tab = createMaterialTopTabNavigator()
+   const  App = () =>{
+  
   
   return (<>
-    {/* <View style={styles.container}>
-     <Text style={{fontSize:30}}>hi expo!</Text> 
-      <StatusBar backgroundColor="purple" />
-      <Text>Platform:{Platform.OS}</Text>
-   
-    </View> */}
     <NavigationContainer>
-    <Stack.Navigator  
-     screenOptions={{
-        headerStyle:{
-          backgroundColor: 'green',
-             /* for all screen header design chage */
-        }
-      }}>
-      <Stack.Screen name="Homescreen" component={Homescreen} />
-      <Stack.Screen name="Flatlist" component={Flatlist}   
-       options={{
-        headerTitle:()=><Button   onPress={app} title="left"/>,
-        headerStyle:{
-          backgroundColor: 'green',
-          /* for signle screen header design chage */
-        }
-      }}/>
-    </Stack.Navigator>
-  </NavigationContainer>
+    <Tab.Navigator  >
+    <Tab.Screen name="Homescreen" component={Homescreen} />
+    <Tab.Screen name="Flatlist" component={Flatlist}   />
+    </Tab.Navigator>
+    </NavigationContainer>
 </>
   );
 }
@@ -59,4 +33,4 @@ const styles = StyleSheet.create({
  
 });
 
-
+export default App;
