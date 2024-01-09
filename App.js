4054fd2a41d9059c33a2de29a6c6f07559a89008
RaoAhmad <ahmadv308@gmail.com>
2024-01-09 16:46:37 +0500

    tab.style

--- App.js
+++ App.js
@@ -1,22 +1,47 @@
-/* import { StatusBar } from 'expo-status-bar'; */
-import { StyleSheet, Text, View ,StatusBar , Platform} from 'react-native';
-/* import Flexbox from './components/Flexbox */
 
-/* import Flatlist from './components/Flatlist';
-import Showhide from './components/Showhide'; */
+import { StyleSheet, Text, View, Button, FlatList} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Flatlist from './components/Flatlist';
+
+
+
+
+
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-     {/*  <Text style={{fontSize:30}}>hi expo!</Text> */}
+  const app=()=>{
+    console.warn('hello app');
+  }
+  
+  return (<>
+    {/* <View style={styles.container}>
+     <Text style={{fontSize:30}}>hi expo!</Text> 
       <StatusBar backgroundColor="purple" />
       <Text>Platform:{Platform.OS}</Text>
-    {/*  <Flatlist/> */}
-{/*  <Showhide/> */}
-
-{/* <Flexbox/> */}
-    </View>
-  
-
+   
+    </View> */}
+    <NavigationContainer>
+    <Stack.Navigator  
+     screenOptions={{
+        headerStyle:{
+          backgroundColor: 'green',
+             /* for all screen header design chage */
+        }
+      }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Flatlist" component={Flatlist}   
+       options={{
+        headerTitle:()=><Button   onPress={app} title="left"/>,
+        headerStyle:{
+          backgroundColor: 'green',
+          /* for signle screen header design chage */
+        }
+      }}/>
+    </Stack.Navigator>
+  </NavigationContainer>
+</>
   );
 }
 
@@ -32,3 +57,15 @@ const styles = StyleSheet.create({
     },
  
 });
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Flatlist"
+        onPress={() => navigation.navigate("Flatlist",{name: "Ahmad"})}
+      />
+    </View>
+  );
+}
