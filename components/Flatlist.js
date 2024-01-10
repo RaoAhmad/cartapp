dc43e651f8f48419c4513680c1c5750171830917
RaoAhmad <ahmadv308@gmail.com>
2024-01-10 17:24:27 +0500

    api-test

--- components/Flatlist.js
+++ components/Flatlist.js
@@ -1,11 +1,11 @@
 import { useEffect } from "react"
-import {Text, View, StyleSheet, FlatList, Image} from "react-native"
+import {Text, View, StyleSheet, FlatList, Image, ActivityIndicator} from "react-native"
 import {useState} from "react";
 export const Flatlist = (props) => {
     //getting props from Homescreen component
     console.warn(props.route.params)
 
-    const employees=[
+    /* const employees=[
         {
            id:1,
            name: 'Ashraf',
@@ -22,18 +22,45 @@ export const Flatlist = (props) => {
 
 
 }
-]
+] */
 //api data fetching
-const [data, setData] = useState('');
+const [data, setData] = useState({});
+const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 const Getdata = async () =>{
-    const url ='https://fakestoreapi.com/products'
-    const result = await fetch(url)
-    const data = await result.json(result)
+    try { const url = "http://192.168.43.137:3000/users"
+    let result = await fetch(url)
+    const data = await result.json()
  setData(data)
+ setLoading
+ 
+} catch (error) {
+    setError(error);
+    setLoading(false);
 }
+  };
+
+
 useEffect(()=>{
     Getdata();
 },[])
+
+if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Error loading data</Text>
+      </View>
+    );
+  }
+
   return (
    <View style={{paddingTop:20}}>
 <Text>this is flat List</Text>
@@ -42,10 +69,9 @@ useEffect(()=>{
 data={data}
 renderItem={({item})=>
 <View>
-    <Text style={{fontSize:20}}>{item.title}</Text>
-    <Text style={{fontSize:15}}>{item.description}</Text>
-  {/*   <Image style={{fontSize:10}}>Price:${item.image}</Image> */}
-    <Text style={{fontSize:10}}>Price:${item.price}</Text>
+    <Text style={{fontSize:20}}>{item}</Text>
+    <Text style={{fontSize:15}}>{}</Text>
+   
 </View>}
 
 keyExtractor={(item) => item.id.toString()}
