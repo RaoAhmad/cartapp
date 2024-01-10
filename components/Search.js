a13c2c55f3c2183315c9dbcad1564af1736996bd
RaoAhmad <ahmadv308@gmail.com>
2024-01-10 17:24:04 +0500

    search-add

---
+++ components/Search.js
@@ -0,0 +1,14 @@
+import {Text, View, TextInput} from 'react-native'
+
+
+const Search = () => {
+  return (
+    <View style={{flex:1}}>
+
+        <TextInput/>
+        <Text>Search</Text>
+    </View>
+  )
+}
+
+export default Search
