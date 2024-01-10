f5e2da95dc93a7f60989e7f92e3e618c7ac95463
RaoAhmad <ahmadv308@gmail.com>
2024-01-10 18:16:41 +0500

    search-function-add

--- components/Search.js
+++ components/Search.js
@@ -2,11 +2,18 @@ import {Text, View, TextInput} from 'react-native'
 
 
 const Search = () => {
+const Searchuser=(text)=>{
+  const url =""
+}
   return (
-    <View style={{flex:1}}>
+    <View style={{flex:1, justifyContent:"top", marginTop:20}}>
 
-        <TextInput/>
-        <Text>Search</Text>
+        <TextInput     
+         placeholder=' search...'
+          style={{ borderColor:"skyblue", borderWidth:2 }} 
+        onChange={(text)=>Searchuser(text)}
+        />
+        <Text style={{textAlign:"center"}}>Search</Text>
     </View>
   )
 }
