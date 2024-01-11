804862e2146d1d704e5a80797f598c108bbe110f
RaoAhmad <ahmadv308@gmail.com>
2024-01-11 18:01:36 +0500

    search-api-ok

--- components/Search.js
+++ components/Search.js
@@ -1,21 +1,37 @@
-import {Text, View, TextInput} from 'react-native'
-
+import React, { useState } from 'react';
+import { Text, View, TextInput } from 'react-native';
 
 const Search = () => {
-const Searchuser=(text)=>{
-  const url =""
-}
+  const [data, setData] = useState([]);
+
+  const searchUser = async (text) => {
+    const url = `https://fakestoreapi.com/users?q=${text}`;
+    let result = await fetch(url);
+    let dat = await result.json();
+  
+    if (dat) {
+      setData(dat);
+    }
+  };
+
   return (
-    <View style={{flex:1, justifyContent:"top", marginTop:20}}>
+    <View style={{ flex: 1, justifyContent: 'top', marginTop: 20 }}>
+      <TextInput
+        placeholder="Search..."
+        style={{ borderColor: 'skyblue', borderWidth: 2 }}
+        onChangeText={(text) => searchUser(text)}
+      />
+      <Text style={{ textAlign: 'center' }}>Search</Text>
 
-        <TextInput     
-         placeholder=' search...'
-          style={{ borderColor:"skyblue", borderWidth:2 }} 
-        onChange={(text)=>Searchuser(text)}
-        />
-        <Text style={{textAlign:"center"}}>Search</Text>
+      {data.length
+        ? data.map((item, index) => (
+            <View key={index}>
+              <Text>{item.name.firstname}</Text>
+            </View>
+          ))
+        : null}
     </View>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
