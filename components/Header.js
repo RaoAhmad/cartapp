6cd638e105986db83971c27a2056e2b2f5943c3a
RaoAhmad <ahmadv308@gmail.com>
2024-01-13 19:10:26 +0500

    header-for-add-to-cart

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import {Text, View, Button } from 'react-native'
+
+
+const Header = () => {
+  
+  return (
+<View>
+    <Text  style={{textAlign:'right', fontSize:30, backgroundColor:'orange', paddingTop:10 }}>0</Text>
+</View>
+  )
+}
+
+export default Header
