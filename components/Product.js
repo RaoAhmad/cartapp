9146535309d0ff38b9fe03dc0e0bc3c0449eeb0e
RaoAhmad <ahmadv308@gmail.com>
2024-01-20 11:35:50 +0500

    remove-to cart-add

--- components/Product.js
+++ components/Product.js
@@ -1,19 +1,47 @@
 import {Text, View, Button } from 'react-native'
-import { addToCart } from '../redux/action'
-import { useDispatch } from 'react-redux'
+import { addToCart , removeFromCart } from '../redux/action'
+import { useDispatch ,useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 const Product = (props) => {
-  const item =props.item
-  const dispacth =useDispatch();
-  const handleAddToCart = (item)=> {
-dispacth(addToCart(item))
+  const product =props.item
 
+  
+
+  /* console.warn (props.item.name)     */          
+  const dispatch =useDispatch();
+  const handleAddToCart = (product)=> {
+dispatch(addToCart(product))
+/* console.warn(product) */
   }
+  const handleRemoveFromCart = (product)=> {
+  dispatch(removeFromCart(product.name))
+}
+
+  cartItems = useSelector((state)=> state.reducer)
+
+const [isAdded, setIsAdded] = useState(false)
+
+  useEffect(()=>{
+ if( cartItems && cartItems.length){
+  cartItems.forEach((cartItem)=>{
+if (cartItem.name === product.name){
+setIsAdded(true);
+}
+  })
+ }
+  },[cartItems])
+
   return (<>
-    <View  style={{ alignItems:'center', borderBottomColor:'pink', borderBottomWidth:2, padding:10 }} key={item.name} >
-    <Text style={{fontSize:30 , textAlign:'center'}}>{item.name}</Text>
-    <Text style={{fontSize:30, textAlign:'center'}} >{item.price}</Text>
-    <Text style={{ fontSize:30, textAlign:'center'}}>{item.color}</Text>
-    <Button  title='Add to Cart' onPress={()=>handleAddToCart(item) }/>
+    <View  style={{ alignItems:'center', borderBottomColor:'pink', borderBottomWidth:2, padding:10 }} key={product.name} >
+    <Text style={{fontSize:30 , textAlign:'center'}}>{product.name}</Text>
+    <Text style={{fontSize:30, textAlign:'center'}} >{product.price}</Text>
+    <Text style={{ fontSize:30, textAlign:'center'}}>{product.color}</Text>
+    {
+
+      isAdded ?  <Button  title='Remove to Cart' onPress={()=>handleRemoveFromCart(product)}/>
+      :  <Button  title='Add to Cart' onPress={()=>handleAddToCart(product)}/>
+    }
+   
   </View>
   </>)
 }
