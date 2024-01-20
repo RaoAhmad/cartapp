d24eb7e47a55a3df5741b0a792e78c17bf6dbe09
RaoAhmad <ahmadv308@gmail.com>
2024-01-20 11:35:24 +0500

    getting-data-from-store

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,44 @@
+/* /* 
 import {Text, View, Button } from 'react-native'
+import {useSelector} from 'react-redux'
+import React, { useState, useEffect} from 'react'
 
 
 const Header = () => {
-  
+ const cartData = useSelector((state)=>state.reducer)
+
+ const [cartItems, setCartItems] = useState( 0)
+console.warn(cartItems)
+ useEffect(()=>{
+  console.warn('Cart data changed. New length:', cartData.length);
+  setCartItems(cartData.length)
+ },[cartData])
   return (
 <View>
-    <Text  style={{textAlign:'right', fontSize:30, backgroundColor:'orange', paddingTop:10 }}>0</Text>
+    <Text  style={{textAlign:'right', fontSize:30, backgroundColor:'orange', paddingTop:10 }}>{cartItems}</Text>
 </View>
   )
 }
 
-export default Header
+export default Header */
+
+import {Text, View, Button } from 'react-native'
+import {useSelector} from 'react-redux'
+import React, { useState, useEffect} from 'react'
+export default  Header = () => {
+  const cartData = useSelector((state) => state.reducer);
+  const [cartItems, setCartItems] = useState(0);
+;
+
+  useEffect(() => {
+    setCartItems(cartData.length);
+  }, [cartData]);
+
+  return (
+    <View>
+      <Text style={{ textAlign: 'right', fontSize: 30, backgroundColor: 'orange', paddingTop: 10 }}>{cartItems}</Text>
+
+    </View>
+  );
+};
+ 
