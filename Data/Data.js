b6c832183c7c9351e92eb05a1c98f409797ae456
RaoAhmad <ahmadv308@gmail.com>
2024-01-22 14:16:48 +0500

    data-add

---
+++ Data/Data.js
@@ -0,0 +1,74 @@
+export const medicanData = [
+  {
+    id: 1,
+    name: "PainAway Tablets",
+    description:
+      "Effective pain relief tablets for various conditions. Helps alleviate pain and discomfort.",
+    rating: 4.5,
+    price: 19.99,
+    image: require("../assets/Images/PainAway-Tablets.png"),
+  },
+  {
+    id: 2,
+    name: "CoughRelief Syrup",
+    description:
+      "Soothing syrup to relieve cough and throat irritation. Provides fast and effective relief.",
+    rating: 4.7,
+    price: 24.99,
+    image: require("../assets/Images/acefyl.png"),
+  },
+  {
+    id: 3,
+    name: "AllergyGuard Nasal Spray",
+    description:
+      "Nasal spray for allergy relief. Helps reduce nasal congestion and allergy symptoms.",
+    rating: 4.2,
+    price: 29.99,
+    image: require("../assets/Images/acefyl.png"),
+  },
+  {
+    id: 4,
+    name: "Cold & Flu Relief Capsules",
+    description:
+      "Powerful capsules to relieve cold and flu symptoms. Provides fast relief and boosts immunity.",
+    rating: 4.0,
+    price: 14.99,
+    image: require("../assets/Images/acefyl.png"),
+  },
+  {
+    id: 5,
+    name: "ImmuneBoost Multivitamin",
+    description:
+      "Multivitamin tablets to boost the immune system. Supports overall health and well-being.",
+    rating: 4.8,
+    price: 39.99,
+    image: require("../assets/Images/acefyl.png"),
+  },
+  {
+    id: 6,
+    name: "DigestEase Probiotics",
+    description:
+      "Probiotic supplements for digestive health. Supports a healthy gut and digestion.",
+    rating: 4.8,
+    price: 39.99,
+    image: require("../assets/Images/acefyl.png"),
+  },
+  {
+    id: 7,
+    name: "HeartCare Omega-3 Capsules",
+    description:
+      "Omega-3 capsules for heart health. Supports cardiovascular function and overall heart health.",
+    rating: 4.8,
+    price: 39.99,
+    image: require("../assets/Images/acefyl.png"),
+  },
+  {
+    id: 8,
+    name: "SleepWell Herbal Tea",
+    description:
+      "Herbal tea blend to promote relaxation and improve sleep quality. Caffeine-free and soothing.",
+    rating: 4.8,
+    price: 39.99,
+    image: require("../assets/Images/acefyl.png"),
+  },
+];
