68a1798fc7f0d6ac4ecd0a524c7ae77d601aeca9
RaoAhmad <ahmadv308@gmail.com>
2024-01-22 20:38:59 +0500

    image-path-change

--- Data/Data.js
+++ Data/Data.js
@@ -6,7 +6,7 @@ export const medicanData = [
       "Effective pain relief tablets for various conditions. Helps alleviate pain and discomfort.",
     rating: 4.5,
     price: 19.99,
-    image: require("../assets/Images/PainAway-Tablets.png"),
+    image: require("../images/acefyl.png"),
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ export const medicanData = [
       "Soothing syrup to relieve cough and throat irritation. Provides fast and effective relief.",
     rating: 4.7,
     price: 24.99,
-    image: require("../assets/Images/acefyl.png"),
+    image: require("../images/acefyl.png"),
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ export const medicanData = [
       "Nasal spray for allergy relief. Helps reduce nasal congestion and allergy symptoms.",
     rating: 4.2,
     price: 29.99,
-    image: require("../assets/Images/acefyl.png"),
+    image: require("../images/acefyl.png"),
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ export const medicanData = [
       "Powerful capsules to relieve cold and flu symptoms. Provides fast relief and boosts immunity.",
     rating: 4.0,
     price: 14.99,
-    image: require("../assets/Images/acefyl.png"),
+    image: require("../images/acefyl.png"),
   },
   {
     id: 5,
@@ -42,7 +42,7 @@ export const medicanData = [
       "Multivitamin tablets to boost the immune system. Supports overall health and well-being.",
     rating: 4.8,
     price: 39.99,
-    image: require("../assets/Images/acefyl.png"),
+    image: require("../images/acefyl.png"),
   },
   {
     id: 6,
@@ -51,7 +51,7 @@ export const medicanData = [
       "Probiotic supplements for digestive health. Supports a healthy gut and digestion.",
     rating: 4.8,
     price: 39.99,
-    image: require("../assets/Images/acefyl.png"),
+    image: require("../images/acefyl.png"),
   },
   {
     id: 7,
@@ -60,7 +60,7 @@ export const medicanData = [
       "Omega-3 capsules for heart health. Supports cardiovascular function and overall heart health.",
     rating: 4.8,
     price: 39.99,
-    image: require("../assets/Images/acefyl.png"),
+    image: require("../images/acefyl.png"),
   },
   {
     id: 8,
@@ -69,6 +69,6 @@ export const medicanData = [
       "Herbal tea blend to promote relaxation and improve sleep quality. Caffeine-free and soothing.",
     rating: 4.8,
     price: 39.99,
-    image: require("../assets/Images/acefyl.png"),
+    image: require("../images/acefyl.png"),
   },
 ];
