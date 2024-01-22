be04e5244d45e16c4b5ad0c2e93dcdf12280ab1d
RaoAhmad <ahmadv308@gmail.com>
2024-01-22 20:38:46 +0500

    render-data-from-home-flatlist

--- components/ProductCart.js
+++ components/ProductCart.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Text, View, StyleSheet, Image } from 'react-native';
 
-const ProductCart = () => {
+const ProductCart = (props) => {
+  const {item } = props;
+
   return (
     <View style={styles.box}>
-      <Text>this product detail</Text>
+     
       <Image style={styles.image} source={require("../images/acefyl.png")} />
-      <Text>this is text</Text>
+      <Text style={styles.currency}>${item.price}</Text>
+      <Text style={styles.title}> Acefyl</Text>
     </View>
   );
 }
@@ -17,9 +20,18 @@ const styles = StyleSheet.create({
   box: {
     flex: 1,
     alignItems: 'center',
+    padding:20,
+    margin: 5,
   },
   image: {
     width: 150,
     height: 150,
-  }
+  },
+  title: {
+    fontSize: 24,
+  fontWeight: 'bold',
+  },
+  currency: {
+    fontSize: 18,
+  },
 });
