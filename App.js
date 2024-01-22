7cbaad2c949e8d091ef911e744bec04e9206bfe8
RaoAhmad <ahmadv308@gmail.com>
2024-01-22 12:24:38 +0500

    navigatinon-container-add

--- App.js
+++ App.js
@@ -1,46 +1,21 @@
 import React from 'react';
-import { Text, View, Button, StyleSheet, ScrollView } from 'react-native';
-import  store  from './redux/store'; 
-import Product from './components/Product';
-import Header from './components/Header';
-import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text, View, Button, StyleSheet} from 'react-native';
+import AppNavigation from './Navigation/AppNavigation';
+
 
 const App = () => {
-  const product = [
-    {
-      name: 'oppo',
-      color: 'red',
-      price: 20,
-      
-    },
-    {
-      name: 'vivo',
-      color: 'Black',
-      price: 30,
-      
-    },
-    {
-      name: 'apple',
-      color: 'gray',
-      price: 40,
-      
-    },
-  ];
+  
 
   return (
-    <>
-    <Provider store={store}>
-        <View style={styles.container}>
-          <Text style={{ paddingTop: 50 }}>This is a UI component</Text>
-          <Header />
-          <ScrollView>
-            {product.map((item,index) => (
-              <Product key={index} item={item} />
-            ))}
-          </ScrollView>
-        </View>
-   </Provider>
-    </>
+   
+   
+   <NavigationContainer>
+      <AppNavigation/>
+    </NavigationContainer>
+
+
   );
 };
 
