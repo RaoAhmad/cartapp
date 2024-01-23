253b3048a136cd7e08b93bbeef99a92bfbc48963
RaoAhmad <ahmadv308@gmail.com>
2024-01-23 13:19:36 +0500

    suggested-products-added

---
+++ Data/suggestProduct.js
@@ -0,0 +1,28 @@
+export const suggestProduct = [
+    {
+      id: 1,
+      name: "PainAway",
+      description:
+        "Effective pain relief tablets for various conditions. Helps alleviate pain and discomfort.",
+      rating: 4.5,
+      price: 19.99,
+      image: require("../images/acefyl.png"),
+    },
+    {
+      id: 2,
+      name: "CoughRelief",
+      description:
+        "Soothing syrup to relieve cough and throat irritation. Provides fast and effective relief.",
+      rating: 4.7,
+      price: 24.99,
+      image: require("../images/acefyl.png"),
+    },
+    {
+      id: 3,
+      name: "AllergyGuard",
+      description:
+        "Nasal spray for allergy relief. Helps reduce nasal congestion and allergy symptoms.",
+      rating: 4.2,
+      price: 29.99,
+      image: require("../images/acefyl.png"),
+    }]
