c0a05aca1fe03c0a1d7fc5f2538e22e8b2957d81
RaoAhmad <ahmadv308@gmail.com>
2024-01-31 08:02:59 +0500

    .

--- Navigation/AppNavigation.js
+++ Navigation/AppNavigation.js
@@ -4,8 +4,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home'
 import  ProductDetails from '../screens/ProductDetails'
 import Medicare from '../screens/Medicare';
+import Header from '../screens/Header';
 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 const AppNavigation = () => {
 
   return (
@@ -16,7 +19,7 @@ const AppNavigation = () => {
     <Stack.Screen name="Medicare" component={Medicare} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="ProductDetails" component={ProductDetails}   
-      />
+        />
     </Stack.Navigator>
   
  
